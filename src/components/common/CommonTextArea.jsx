9c1a670d9d8f01dab0a3d0384e5504a7211efe11
Mitali Gohel <mitaligohel.bs@gmail.com>
2026-01-26 10:50:19 +0530

    add validation in form

--- src/components/common/CommonTextArea.jsx
+++ src/components/common/CommonTextArea.jsx
@@ -1,12 +1,21 @@
-const CommonTextArea = ({ title, value, type = "text", onChange }) => (
+const CommonTextArea = ({
+  title,
+  type = "text",
+  placeholder,
+  name,
+  error,
+  ...props
+}) => (
   <div className="flex flex-col">
     <label>{title}</label>
     <textarea
       type={type}
-      className="border border-[#9F9F9F] rounded-sm h-20"
-      value={value}
-      onChange={onChange}
+      className="border border-[#9F9F9F] rounded-sm h-20 placeholder:text-start p-5 leading-10 focus:outline-none"
+      placeholder={placeholder}
+      name={name}
+      {...props}
     />
+    <p className="text-sm mt-1 text-red-500">{error}</p>
   </div>
 );
 
