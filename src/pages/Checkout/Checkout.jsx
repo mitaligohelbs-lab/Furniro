dca7dab9e14f0313826eb498b1380e75fbc32e4c
Mitali Gohel <mitaligohel.bs@gmail.com>
2026-01-26 14:23:52 +0530

    minor fix

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -4,14 +4,12 @@ import Service from "../Shop/component/Service";
 import CheckoutImage from "../../assets/checkout.png";
 import BillingInformation from "./components/BillingInformation";
 
-const Checkout = () => {
-  return (
-    <div>
-      <HeaderImage src={CheckoutImage} />
-      <BillingInformation />
-      <Service />
-    </div>
-  );
-};
+const Checkout = () => (
+  <div>
+    <HeaderImage src={CheckoutImage} />
+    <BillingInformation />
+    <Service />
+  </div>
+);
 
 export default Checkout;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,14 +4,12 @@ import Service from "../Shop/component/Service";
 
 import ContactImage from "../../assets/contact.png";
 
-const Contact = () => {
-  return (
-    <>
-      <HeaderImage src={ContactImage} />
-      <ContactInformation />
-      <Service />
-    </>
-  );
-};
+const Contact = () => (
+  <div>
+    <HeaderImage src={ContactImage} />
+    <ContactInformation />
+    <Service />
+  </div>
+);
 
 export default Contact;
